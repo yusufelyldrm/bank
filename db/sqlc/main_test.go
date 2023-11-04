ffce2e9de04ce9c27d24cf3372015636ae66b17a
yusufelyldrm <yusufelyildirim@gmail.com>
2023-11-05 01:53:17 +0300

    pulling id with command

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,23 +2,37 @@ package db
 
 import (
 	"database/sql"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
-
-	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
-var testDB *sql.DB 
+var testDB *sql.DB
 
 const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@192.168.215.2:5432/simple_bank?sslmode=disable"
+	dbDriver    = "postgres"
+	dbSourceEnv = "POSTGRES_IP"
 )
 
 func TestMain(m *testing.M) {
-	var err error 
+	var err error
+
+	cmd := exec.Command("docker", "inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "postgres")
+	cmdOutput, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal("Failed to execute 'docker inspect' command:", err)
+	}
+
+	dbSourceIP := strings.TrimSpace(string(cmdOutput))
+	if dbSourceIP == "" {
+		log.Fatal("Failed to get PostgreSQL container IP")
+	}
+
+	dbSource := "postgresql://root:secret@" + dbSourceIP + ":5432/simple_bank?sslmode=disable"
 
 	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
