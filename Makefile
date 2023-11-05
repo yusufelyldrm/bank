08c4a2fd1fb89885f3347deb23b921a45a3c6e8d
Yusuf Ziya Elyıldırım <120419565+yusufelyldrm@users.noreply.github.com>
2023-11-05 22:44:58 +0300

    Add ci.yml (#6)

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 createdb: 
-	docker exec -it postgres12 createdb --username=root --owner=root simple_bank
+	docker exec -it postgres createdb --username=root --owner=root simple_bank
 	
 dropdb: 
-	docker exec -it postgres12 dropdb simple_bank
+	docker exec -it postgres dropdb simple_bank
 
 postgres: 
-	docker run --name postgres12 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:latest
+	docker run --name postgres -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:latest
 	
 migrateup:
 	migrate -path db/migration -database "postgresql://root:secret@192.168.215.2:5432/simple_bank?sslmode=disable" -verbose up
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,23 +2,45 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
-
-	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
-var testDB *sql.DB 
+var testDB *sql.DB
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@192.168.215.2:5432/simple_bank?sslmode=disable"
 )
 
 func TestMain(m *testing.M) {
-	var err error 
+	var err error
+
+	dockerContainerNameCmd := exec.Command("docker", "ps", "--format", "{{.Names}}")
+	dockerContainerNameOutput, err := dockerContainerNameCmd.CombinedOutput()
+	if err != nil {
+		log.Fatal("Failed to execute 'docker ps' command:", err)
+	}
+	dockerContainerName := strings.TrimSpace(string(dockerContainerNameOutput))
+	fmt.Println("Docker container name:", dockerContainerName)
+
+	cmd := exec.Command("docker", "inspect", "-f", "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}", dockerContainerName)
+	cmdOutput, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal("Failed to execute 'docker inspect' command:", err)
+	}
+
+	dbSourceIP := strings.TrimSpace(string(cmdOutput))
+	if dbSourceIP == "" {
+		log.Fatal("Failed to get PostgreSQL container IP")
+	}
+
+	dbSource := "postgresql://root:secret@" + dbSourceIP + ":5432/simple_bank?sslmode=disable"
 
 	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
